3e442b4442975a3a78afa00a93dfc33a47516524
Jerry <g3rard0.liv3@gmail.com>
2022-06-23 21:49:34 -0500

    configuracion migracda correctamente

---
+++ ms.admin.server.admin/src/main/java/mx/com/ms/ecommerce/admin/server/admin/Application.java
@@ -0,0 +1,18 @@
+package mx.com.ms.ecommerce.admin.server.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
